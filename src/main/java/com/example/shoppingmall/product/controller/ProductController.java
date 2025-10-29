4bb34c063be221671aad8c8122ed6a00be5d3f53
jungwonseok <dnjsehfdld@naver.com>
2025-10-29 16:07:47 +0900

    [refactor] Product API 불필요한 import문 제거

--- src/main/java/com/example/shoppingmall/product/controller/ProductController.java
+++ src/main/java/com/example/shoppingmall/product/controller/ProductController.java
@@ -2,13 +2,11 @@ package com.example.shoppingmall.product.controller;
 
 import com.example.shoppingmall.product.dto.ProductResponse;
 import com.example.shoppingmall.product.dto.ProductSearchRequest;
-import com.example.shoppingmall.product.entity.Product;
 import com.example.shoppingmall.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
--- src/main/java/com/example/shoppingmall/product/service/ProductService.java
+++ src/main/java/com/example/shoppingmall/product/service/ProductService.java
@@ -7,7 +7,6 @@ import com.example.shoppingmall.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
