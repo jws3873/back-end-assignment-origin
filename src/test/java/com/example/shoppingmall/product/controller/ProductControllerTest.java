1de82add0f4b78757d90a1b496f6d75eda71ea9a
jungwonseok <dnjsehfdld@naver.com>
2025-11-01 03:00:08 +0900

    [test] 상품 API 컨트롤러 단위테스트 @AutoConfigureMockMvc 애너테이션 추가

--- src/test/java/com/example/shoppingmall/product/controller/ProductControllerTest.java
+++ src/test/java/com/example/shoppingmall/product/controller/ProductControllerTest.java
@@ -5,6 +5,7 @@ import com.example.shoppingmall.product.service.ProductService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
@@ -22,6 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ProductController.class)
+@AutoConfigureMockMvc(addFilters = false)
 public class ProductControllerTest {
 
     @Autowired
